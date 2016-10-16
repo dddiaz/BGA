644b78c03b56b8d105dbb22bb588197f1427c1fa
Daniel Diaz <daniel.delvin.diaz@gmail.com>
2016-10-16 15:56:58 -0700

    last bg finally working

--- web/app.py
+++ web/app.py
@@ -3,6 +3,7 @@ from flask import Flask, redirect, url_for, request, render_template
 from pymongo import MongoClient
 import configparser
 import json
+from bson.json_util import dumps
 
 app = Flask(__name__)
 
@@ -17,33 +18,31 @@ MONGO_USER = config['nightscout-mongodb']['MONGO_USER']
 MONGO_USER_PASSWORD = config['nightscout-mongodb']['MONGO_USER_PASSWORD']
 MONGO_HOST = config['nightscout-mongodb']['MONGO_HOST']
 MONGO_PORT = config['nightscout-mongodb']['MONGO_PORT']
-MONGO_PORT = config.getint('nightscout-mongodb', 'MONGO_PORT')
 MONGO_DB = config['nightscout-mongodb']['MONGO_DB']
 ### Standard URI format: mongodb://[dbuser:dbpassword@]host:port/dbname
-MONGODB_URI = ''.join(['mongodb://', 'MONGO_USER', ':', 'MONGO_USER_PASSWORD',
-                        '@', 'MONGO_HOST', ':', str(MONGO_PORT), '/', MONGO_DB])
-# MONGODB_URI = 'mongodb://' + MONGO_USER + ':' + MONGO_USER_PASSWORD + '@' + MONGO_HOST \
-#               + ':' + MONGO_PORT + '/' + MONGO_DB
+MONGODB_URI = ''.join(['mongodb://', MONGO_USER, ':', MONGO_USER_PASSWORD,
+                        '@', MONGO_HOST, ':', str(MONGO_PORT), '/', MONGO_DB])
 
-### Standard URI format: mongodb://[dbuser:dbpassword@]host:port/dbname
 print(MONGODB_URI)
 client = MongoClient(MONGODB_URI)
-# BG db is labled entries
-bg = client.entries
+
+db = client.get_default_database()
+
+entries = db['entries']
 
 
 @app.route('/')
 def hello_world():
-    return 'Flask Dockerized'
+    return 'BGA Web API'
 
 
 @app.route('/lastbg')
 def lastbg():
-    if client is not None:
-        doc = bg.find().limit(1).sort({'$natural':-1})
-        # to do check database is set up
-        if doc is not None:
-            return json.dumps(doc, sort_keys=True, indent=4)
+    if entries is not None:
+        cursor = entries.find().sort([("date", -1)]).limit(1)
+        # todo check database is set up and connected to
+        # also what happens if no values in db
+        return dumps(cursor)
 
 
 if __name__ == '__main__':
